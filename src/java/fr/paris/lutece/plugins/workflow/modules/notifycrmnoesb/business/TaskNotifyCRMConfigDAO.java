211b6e1898317e06aa2337e26a02a2b422bb6370
AlexandreClose <alexandre.close@gmail.com>
2017-01-12 16:56:26 +0100

    MWNNESB-1 - Initial work

---
+++ src/java/fr/paris/lutece/plugins/workflow/modules/notifycrmnoesb/business/TaskNotifyCRMConfigDAO.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.workflow.modules.notifycrmnoesb.business;
+
+import fr.paris.lutece.plugins.workflow.modules.notifycrmnoesb.service.NotifyCRMNoEsbPlugin;
+import fr.paris.lutece.plugins.workflowcore.business.config.ITaskConfigDAO;
+import fr.paris.lutece.plugins.workflow.modules.notifycrmnoesb.business.TaskNotifyCRMConfig;
+import fr.paris.lutece.util.sql.DAOUtil;
+
+
+/**
+ *
+ * TaskNotifyCRMConfigDAO
+ *
+ */
+public class TaskNotifyCRMConfigDAO implements ITaskConfigDAO<TaskNotifyCRMConfig>
+{
+    private static final String SQL_QUERY_FIND_BY_PRIMARY_KEY = " SELECT id_task, status_text, sender, object, message, is_demand_crm_creation FROM task_notify_crm_no_esb_cf  WHERE id_task = ? ";
+    private static final String SQL_QUERY_INSERT = " INSERT INTO task_notify_crm_no_esb_cf( id_task, status_text, sender, object, message, is_demand_crm_creation ) VALUES ( ?,?,?,?,?,? ) ";
+    private static final String SQL_QUERY_UPDATE = "UPDATE task_notify_crm_no_esb_cf SET id_task = ?, status_text = ?,sender = ?, object = ?, message = ?, is_demand_crm_creation = ? WHERE id_task = ? ";
+    private static final String SQL_QUERY_DELETE = " DELETE FROM task_notify_crm_no_esb_cf WHERE id_task = ? ";
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public synchronized void insert( TaskNotifyCRMConfig config )
+    {
+        DAOUtil daoUtil = new DAOUtil( SQL_QUERY_INSERT, NotifyCRMNoEsbPlugin.getPlugin(  ) );
+
+        int nIndex = 1;
+
+        daoUtil.setInt( nIndex++, config.getIdTask( ) );
+        daoUtil.setString( nIndex++, config.getStatusText( ) );
+        daoUtil.setString( nIndex++, config.getSender( ) );
+        daoUtil.setString( nIndex++, config.getObject( ) );
+        daoUtil.setString( nIndex++, config.getMessage( ) );
+        daoUtil.setBoolean( nIndex++, config.getDemandCRMCreation());
+
+        daoUtil.executeUpdate(  );
+        daoUtil.free(  );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void store( TaskNotifyCRMConfig config )
+    {
+        DAOUtil daoUtil = new DAOUtil( SQL_QUERY_UPDATE, NotifyCRMNoEsbPlugin.getPlugin(  ) );
+
+        int nIndex = 1;
+
+        daoUtil.setInt( nIndex++, config.getIdTask( ) );
+        daoUtil.setString( nIndex++, config.getStatusText( ) );
+        daoUtil.setString( nIndex++, config.getSender( ) );
+        daoUtil.setString( nIndex++, config.getObject( ) );
+        daoUtil.setString( nIndex++, config.getMessage( ) );
+        daoUtil.setBoolean( nIndex++, config.getDemandCRMCreation( ) );
+
+        daoUtil.setInt( nIndex++, config.getIdTask(  ) );
+        daoUtil.executeUpdate(  );
+        daoUtil.free(  );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public TaskNotifyCRMConfig load( int nIdTask )
+    {
+        TaskNotifyCRMConfig config = null;
+        DAOUtil daoUtil = new DAOUtil( SQL_QUERY_FIND_BY_PRIMARY_KEY, NotifyCRMNoEsbPlugin.getPlugin(  ) );
+
+        daoUtil.setInt( 1, nIdTask );
+
+        daoUtil.executeQuery(  );
+
+        int nIndex = 1;
+
+        if ( daoUtil.next(  ) )
+        {
+            config = new TaskNotifyCRMConfig(  );
+            config.setIdTask( daoUtil.getInt( nIndex++ ) );
+            config.setStatusText(daoUtil.getString( nIndex++ ) );
+            config.setSender(daoUtil.getString( nIndex++ ) );
+            config.setObject(daoUtil.getString( nIndex++ ) );
+            config.setMessage( daoUtil.getString( nIndex++ ) );
+            config.setDemandCRMCreation( daoUtil.getBoolean( nIndex++ )  );
+        }
+
+        daoUtil.free(  );
+
+        return config;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete( int nIdTask )
+    {
+        DAOUtil daoUtil = new DAOUtil( SQL_QUERY_DELETE, NotifyCRMNoEsbPlugin.getPlugin(  ) );
+
+        daoUtil.setInt( 1, nIdTask );
+        daoUtil.executeUpdate(  );
+        daoUtil.free(  );
+    }
+}
---
+++ src/java/fr/paris/lutece/plugins/workflow/modules/notifycrmnoesb/service/IdeationNotifyCRMService.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.workflow.modules.notifycrmnoesb.service;
+
+import fr.paris.lutece.plugins.ideation.business.Idee;
+import fr.paris.lutece.plugins.ideation.business.IdeeHome;
+import fr.paris.lutece.portal.service.prefs.UserPreferencesService;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IdeationNotifyCRMService implements INotifyCRMService
+{
+    private static final String MARK_PSEUDO = "pseudo";
+    private static final String MARK_IDEA_TITLE = "titre_idee";
+    private static final String MARK_IDEA_CREATION_DATE = "date_depot_idee";
+    private static final String MARK_IDEA_REFERENCE = "idee_reference";
+    
+    @Override
+    public List<String> getListMarkers( )
+    {
+        List<String> listMarkers = new ArrayList();
+        listMarkers.add( MARK_PSEUDO );
+        listMarkers.add( MARK_IDEA_TITLE );
+        listMarkers.add( MARK_IDEA_CREATION_DATE );
+        listMarkers.add( MARK_IDEA_REFERENCE );
+        return listMarkers;
+    }
+    
+    @Override
+    public Map<String,Object> fillModelInfoResource( int nIdRessource )
+    {
+        Idee idee = IdeeHome.findByPrimaryKey( nIdRessource );
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put( MARK_PSEUDO, UserPreferencesService.instance( ).getNickname( idee.getLuteceUserName( ) ) );
+        model.put( MARK_IDEA_TITLE , idee.getTitre( ) );
+        model.put( MARK_IDEA_CREATION_DATE, idee.getCreationTimestamp( ) );
+        model.put( MARK_IDEA_REFERENCE, idee.getReference( ) );
+        return model;
+    }
+    
+    @Override
+    public String getIdDemand( int nIdRessource )
+    {
+        return Integer.toString( nIdRessource );
+    }
+    
+    @Override
+    public String getUserGuid( int nIdRessource )
+    {
+        Idee idee = IdeeHome.findByPrimaryKey( nIdRessource );
+        return idee.getLuteceUserName( );
+    }
+}
